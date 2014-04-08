e2874e75d41bc5981720cd16473e259d6d134e91
preethamv <zombie.dr@gmail.com>
2014-04-08 11:06:17 -0500

    Comments

--- JsonTestApp/Program.cs
+++ JsonTestApp/Program.cs
@@ -11,6 +11,10 @@ namespace JsonTestApp
     {
         static void Main(string[] args)
         {
+            // Location of the output file.
+            const string filePath = @"c:\temp\store.json";
+
+            // Creating a simple dictionary that has a non-string key
             var dictStore = new Dictionary<DictionaryKey, int>();
             for (var i = 0; i < 800; i++)
             {
@@ -18,19 +22,20 @@ namespace JsonTestApp
             }
             var settings = new JsonSerializerSettings { Formatting = Formatting.Indented };
             var jsonSerializer = JsonSerializer.Create(settings);
-            const string filePath = @"c:\temp\store.json";
             using (var stream = new FileStream(filePath, FileMode.Create))
             {
                 var streamWriter = new StreamWriter(stream);
                 jsonSerializer.Serialize(streamWriter, dictStore);
                 streamWriter.Flush();
             }
+
             var stopWatch = Stopwatch.StartNew();
             using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
                 var deserialize = jsonSerializer.Deserialize(new StreamReader(stream), typeof(Dictionary<DictionaryKey, int>));
             }
             stopWatch.Stop();
+
             Console.WriteLine("Time elapsed: "+stopWatch.ElapsedMilliseconds);
         }
     }
